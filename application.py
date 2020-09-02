244ac72b78137f0a5f68ddaf087299ed3686f859
sisodiya2421 <sisodiya2421@users.noreply.github.com>
2020-09-02 12:02:05 +0000

    autopep8 action fixes

--- application.py
+++ application.py
@@ -3,12 +3,14 @@ import awake
 
 application = Flask(__name__)
 
-@application.route('/', methods = ['GET'])
+
+@application.route('/', methods=['GET'])
 def index():
     if request.method == "POST":
         query = request.form['query']
         awake.get_request(query)
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     application.run(debug=True)
